f37fae46a380bcbd4d54b88396ef98c00396b6f3
Saksham Rathi <22b1003@iitb.ac.in>
2024-11-16 14:55:57 +0530

    actual-jpeg

--- main/actual_jpeg.py
+++ main/actual_jpeg.py
@@ -8,5 +8,8 @@ if image.mode == 'RGBA':
     # Convert RGBA to RGB
     image = image.convert('RGB')
 
+# Convert the image to grayscale
+image = image.convert('L')  # 'L' mode is for grayscale
+
 # Save the image in JPEG format with adjustable quality
 image.save("compressed_image.jpeg", "JPEG", quality=50)  # Quality ranges from 1 (worst) to 95 (best)
